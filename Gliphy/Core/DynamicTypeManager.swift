0f0130e9ea813f260893833dd52b0fbe786e295a
Scott Williams <scott@swilliams.me>
2016-02-16 19:53:40 -0700

    Init

---
+++ Gliphy/Core/DynamicTypeManager.swift
@@ -0,0 +1,13 @@
+//
+//  DynamicTypeManager.swift
+//  Gliphy
+//
+//  Created by Scott Williams on 2/16/16.
+//  Copyright © 2016 Tallwave. All rights reserved.
+//
+
+import UIKit
+
+public struct DynamicTypeManager {
+
+}
