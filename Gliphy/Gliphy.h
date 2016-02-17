205db5ab31ceb67cb257ec76452f57bb8c8b65c2
Scott Williams <scott@swilliams.me>
2016-02-16 19:49:01 -0700

    Initial Commit

---
+++ Gliphy/Gliphy.h
@@ -0,0 +1,19 @@
+//
+//  Gliphy.h
+//  Gliphy
+//
+//  Created by Scott Williams on 2/16/16.
+//  Copyright © 2016 Tallwave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gliphy.
+FOUNDATION_EXPORT double GliphyVersionNumber;
+
+//! Project version string for Gliphy.
+FOUNDATION_EXPORT const unsigned char GliphyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gliphy/PublicHeader.h>
+
+
